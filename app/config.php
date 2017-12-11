1956543d797eec6dbf01a64ef8644dcfe627dcf8
nathan71370 <nathanmercier@orange.fr>
2017-12-11 14:40:10 +0100

    mieux mtn

--- app/config.php
+++ app/config.php
@@ -1,6 +1,6 @@
 <?php
 define("hostname","localhost"); // ou serveurmysql
-define("database","mini_projet");
+define("database","dbname");
 define("username","root");
-define("password","1008");
+define("password","");
 
